d30bcadbf866c2238f44dbeb093b8827f4368df5
Michael Golenia <michaelgolenia1@gmail.com>
2022-10-11 15:35:12 -0300

    Files added: models/base_model.py tests/test_models/test_base_model.py

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" TEST BASE MODEL"""
+
+import unittest
+from models.base_model import BaseModel
+
+class test_base_models(unittest.TestCase):
+    
+    def test_normal_case(self):
+        base = BaseModel()
+        self.assertIsNotNone(base.id)
+        self.assertIsNotNone(base.created_at)
+        self.assertEqual(base.created_at, base.updated_at)
+        base.save()
+        self.assertNotEqual(base.created_at, base.updated_at)
+        self.assertEqual(type(base.to_dict()), type({}))
+        
+if __name__ == '__main__':
+    unittest.main()
