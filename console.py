07e65a3986cacdf411d676ecbc4968e77b005df5
Marcelo <marcelorodb@gmail.com>
2022-10-13 15:55:39 -0700

    first attempt console

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module for the entry point of the command interpreter."""
+
+import cmd
+from models.base_modeL import BaseModel
+from models import storage
+import re
+import json
+
+class HBNBCommand(cmd.Cmd):
+
+    """Class for the command interpreter. """
+
+    prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        """Handles End Of File character.
+        """
+        print()
+        return True
+
+    def do_quit(self, line):
+        """Exits the program.
+        """
+        return True
+
+    def emptyline(self):
+        """Doesn't do anything on ENTER. """
+        pass
