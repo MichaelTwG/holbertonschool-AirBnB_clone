3c8010f65a500f333496e83ebb97190beac60038
MichaelTwG <michaelgolenia1@gmail.com>
2022-10-14 10:48:07 -0300

    test_engine/test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,10 +7,10 @@ import models
 
 class test_file_storage(unittest.TestCase):
     def testFile(self):
-        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+        self.assertEqual(str, type(models.storage.__file_path))
 
     def testObj(self):
-        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+        self.assertEqual(dict, type(models.storage.__objects))
 
     def testAll(self):
         dictionary = models.storage.all()
@@ -24,6 +24,13 @@ class test_file_storage(unittest.TestCase):
     def test_reload(self):
         models.storage.__file_path = "NON_EXIST_FILE"
         self.assertRaises(FileNotFoundError, models.storage.reload())
+
+    def test_save(self):
+        obj = BaseModel()
+        List = models.storage.all()
+        obj.save()
+        List2 = models.storage.all()
+        self.assertNotEqual(len(list), len(List2))
 
 if __name__ == '__main__':
     unittest.main()
